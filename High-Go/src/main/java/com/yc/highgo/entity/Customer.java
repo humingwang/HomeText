f7e05679ea7cac89a712611fed9771988c1f6eb8
humingwang <543919061@qq.com>
2016-09-01 10:07:45 +0800

    修改Customer实体类

--- High-Go/src/main/java/com/yc/highgo/entity/Customer.java
+++ High-Go/src/main/java/com/yc/highgo/entity/Customer.java
@@ -1,5 +1,6 @@
 package com.yc.highgo.entity;
- public class Customer{
+
+public class Customer {
 	private int cid;
 	private String cname;
 	private String cpwd;
@@ -7,87 +8,68 @@ package com.yc.highgo.entity;
 	private String csex;
 	private int cage;
 	private String cemail;
-	
-	public Customer(int cid, String cname, String cpwd, int cmoney, String csex, int cage, String cemail) {
-		super();
-		this.cid = cid;
-		this.cname = cname;
-		this.cpwd = cpwd;
-		this.cmoney = cmoney;
-		this.csex = csex;
-		this.cage = cage;
-		this.cemail = cemail;
-	}
-	
-	public Customer( String cname, String cpwd) {
-		this.cname = cname;
-		this.cpwd = cpwd;
-	}
-	
-	public Customer() {
-	}
-
 	public int getCid() {
 		return cid;
 	}
-
 	public void setCid(int cid) {
 		this.cid = cid;
 	}
-
 	public String getCname() {
 		return cname;
 	}
-
 	public void setCname(String cname) {
 		this.cname = cname;
 	}
-
 	public String getCpwd() {
 		return cpwd;
 	}
-
 	public void setCpwd(String cpwd) {
 		this.cpwd = cpwd;
 	}
-
 	public int getCmoney() {
 		return cmoney;
 	}
-
 	public void setCmoney(int cmoney) {
 		this.cmoney = cmoney;
 	}
-
 	public String getCsex() {
 		return csex;
 	}
-
 	public void setCsex(String csex) {
 		this.csex = csex;
 	}
-
 	public int getCage() {
 		return cage;
 	}
-
 	public void setCage(int cage) {
 		this.cage = cage;
 	}
-
 	public String getCemail() {
 		return cemail;
 	}
-
 	public void setCemail(String cemail) {
 		this.cemail = cemail;
 	}
-
+	public Customer(int cid, String cname, String cpwd, int cmoney, String csex, int cage, String cemail) {
+		this.cid = cid;
+		this.cname = cname;
+		this.cpwd = cpwd;
+		this.cmoney = cmoney;
+		this.csex = csex;
+		this.cage = cage;
+		this.cemail = cemail;
+	}
+	public Customer(String cname, String cpwd) {
+		this.cname = cname;
+		this.cpwd = cpwd;
+	}
+	public Customer() {
+	}
+	
 	@Override
 	public String toString() {
 		return "Customer [cid=" + cid + ", cname=" + cname + ", cpwd=" + cpwd + ", cmoney=" + cmoney + ", csex=" + csex
 				+ ", cage=" + cage + ", cemail=" + cemail + "]";
 	}
 	
-	
- } 
+}
