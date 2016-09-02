23e657466783b5dca29e0abed1518b924e636d4e
qyy0216 <1359379059@qq.com>
2016-09-02 11:56:24 +0800

    后台登录和一些小修改

---
+++ High-Go/src/main/java/com/yc/highgo/service/AdminService.java
@@ -0,0 +1,9 @@
+package com.yc.highgo.service;
+
+import com.yc.highgo.entity.Admin;
+
+public interface AdminService {
+
+	Admin login(Admin admin);
+
+}
---
+++ High-Go/src/test/java/com/yc/highgo/service/Impl/AdminServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.yc.highgo.service.Impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.yc.highgo.entity.Admin;
+import com.yc.highgo.service.AdminService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring.xml")
+public class AdminServiceImplTest {
+
+	@Autowired
+	private AdminService adminService;
+	
+	@Test
+	public void testLogin() {
+		Admin admin=adminService.login(new Admin("sa","a"));
+		System.out.println(admin);
+		assertNotNull(admin);
+	}
+
+}
